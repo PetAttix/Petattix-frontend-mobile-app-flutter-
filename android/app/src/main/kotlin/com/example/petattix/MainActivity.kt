cf284ca9ec33a17f33eff27ca11a97c312098995
Sagor Ahamed <sagorahammed002@gmail.com>
2025-11-20 16:02:06 +0600

    payemnt done dd

--- android/app/src/main/kotlin/com/example/petattix/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.petattix.petattix
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity()
